15c15019b887a3c039f6b95171718f32136ed2c2
anand360 <anandkishore31@gmail.com>
2016-03-21 23:51:57 +0530

    sington pattern commit

---
+++ SingletonPattern/Core/Logger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern.Core
+{
+    public class Logger
+    {
+        private  static Logger _logger;
+
+        private Logger(){}
+
+        public static Logger GetInstance()
+        {
+            if (_logger == null)
+            {
+                _logger = new Logger();
+            }
+
+            return _logger;
+        }
+
+        public void LogMessage(string message)
+        {
+            Console.WriteLine("Log message is {0} @ {1}", message, DateTime.Now);
+        }
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using SingletonPattern.Core;
+
+namespace SingletonPattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = Logger.GetInstance();
+            log.LogMessage("First attempt error");
+            Thread.Sleep(new TimeSpan(0, 0, 10));
+            log.LogMessage("First attempt error");
+            Thread.Sleep(new TimeSpan(0, 0, 10));
+            log.LogMessage("First attempt error");
+
+            Console.ReadKey();
+        }
+    }
+}
